fc1f073d97b6fb4edb86df853a1b03220f11b46e
Juvénal Pengele <juvenalpengele@arolitec.com>
2018-09-28 14:44:40 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <tag-input placeholder="Entrez les tags" name="tags"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TagInput from './components/TagInput.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TagInput
+  }
+}
+</script>
